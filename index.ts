99ba8b14643a984e6313ff877d1c270aee1f7f89
mameru-carr <samuelcarr.official@gmail.com>
2024-09-01 23:25:43 +0530

    Fix CORS Issue

--- index.ts
+++ index.ts
@@ -8,21 +8,26 @@ if (environment === "production") {
 
 const CORS_HEADERS = {
         'Access-Control-Allow-Origin': accessControlAllowOrigins,
-        'Access-Control-Allow-Methods': 'OPTIONS, POST',
-        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, DELETE, HEAD, PATCH, PUT, OPTIONS, POST',
 };
 
 // TODO
-// 1. CORS
-// 2. HTTP File
+// 1. [x] CORS
+// 2. [ ] HTTP File
+// 3. [ ] logs
+// 4. [ ] Error Handling
 
 const server = Bun.serve({
     async fetch(request) {
 
         // Handle CORS preflight requests
         if (request.method === 'OPTIONS') {
-            const res = new Response('Departed', CORS_HEADERS);
-            return res;
+            const response = new Response('Departed', {
+                headers: {
+                    ...CORS_HEADERS
+                }
+            });
+            return response;
         }
 
         const path = new URL(request.url).pathname;
@@ -32,20 +37,23 @@ const server = Bun.serve({
 
         switch(path) {
             case "/wakatime":
-            const username = "@mameru-carr";
-            const endpoint = `users/current/stats`;
-            const url = `${base_url}/${endpoint}`;
-            const response = await fetch(url, {
-                method: "GET",
-                headers : {
-                    Authorization: `Basic ${encodedKey}`,
-                    ...CORS_HEADERS
-                },
-            });
-            const stats: StatsResponse = await response.json();
-            return Response.json(stats);
+                const username = "@mameru-carr";
+                const endpoint = `users/current/stats`;
+                const url = `${base_url}/${endpoint}`;
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers : {
+                        Authorization: `Basic ${encodedKey}`,
+                    },
+                });
+                const stats: StatsResponse = await response.json();
+                return Response.json(stats, {
+                    headers: {
+                        ...CORS_HEADERS
+                    }
+                });
         }
     },
 });
 
-console.log(chalk.green(`listening on PORT: ${server.url}`));
+console.log(chalk.green(`listening on: ${server.url}`));
